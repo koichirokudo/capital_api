4d98a42b0158cf0aaa04cb0a2649cc5d3083ae77
koichirokudo <koichirokudo746@gmail.com>
2024-02-17 12:08:36 +0900

    Supported Japanese for faker. SQL query log can now be output in the local environment. I also installed xdebug.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\DB;
+use Faker\Generator as FakerGenerator;
+use Faker\Factory as FakerFactory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // fakerの日本語化
+        $this->app->singleton(FakerGenerator::class, function () {
+            return FakerFactory::create('ja_JP');
+        });
     }
 
     /**
@@ -20,5 +26,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if ($this->app->environment() === 'local') {
+            DB::listen(function ($query) {
+                \Log::info(
+                    $query->sql,
+                    $query->bindings,
+                    $query->time
+                );
+            });
+        }
     }
 }
