14954f0f9962085c1660bd8034de9d3ce2a4f3b6
koichirokudo <koichirokudo746@gmail.com>
2024-04-23 19:25:05 +0900

    Add logout function.

--- app/Http/Controllers/LogoutController.php
+++ app/Http/Controllers/LogoutController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Log;
 
 class LogoutController extends Controller
@@ -10,7 +11,12 @@ class LogoutController extends Controller
     public function __invoke(Request $request)
     {
         Log::debug('ログアウト処理を実行します');
-        $request->user()->currentAccessToken()->delete();
+        $request->user()->tokens()->delete();
+
+        // クライアント側のセッションをクリア
+        Cookie::queue(Cookie::forget('capital_session'));
+        Cookie::queue(Cookie::forget('XSRF-TOKEN'));
+
         return response()->json(['message' => 'ログアウトしました']);
     }
 }
